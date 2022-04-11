cf3af3472ff155e0bb26461beace4120e42b1975
Stefan Horvath <a@a.a>
2022-04-11 16:53:36 +0200

    remove redundant commented code

--- src/main/java/sk/stuba/fei/uim/oop/gui/BoardSizeSlider.java
+++ src/main/java/sk/stuba/fei/uim/oop/gui/BoardSizeSlider.java
@@ -1,29 +1,3 @@
-// package sk.stuba.fei.uim.oop.gui;
-
-// import javax.swing.JSlider;
-// import javax.swing.event.ChangeEvent;
-// import javax.swing.event.ChangeListener;
-
-// import sk.stuba.fei.uim.oop.controls.GameLogic;
-
-// public class BoardSizeSlider extends JSlider implements ChangeListener {
-//     private GameLogic logic;
-
-//     public BoardSizeSlider(int orientation, int min, int max, int value, GameLogic logic) {
-//         super(orientation, min, max, value);
-//         this.logic = logic;
-//         this.addChangeListener(this);
-// this.setFocusable(false);
-//     }
-
-//     @Override
-//     public void stateChanged(ChangeEvent e) {
-//         if (!((JSlider) e.getSource()).getValueIsAdjusting()) {
-//             this.logic.changeBoardSize(((JSlider) e.getSource()).getValue());
-//         }
-//     }
-// }
-
 package sk.stuba.fei.uim.oop.gui;
 
 import javax.swing.JSlider;
--- src/main/java/sk/stuba/fei/uim/oop/reversi/Direction.java
+++ src/main/java/sk/stuba/fei/uim/oop/reversi/Direction.java
@@ -1,24 +1,3 @@
-// package sk.stuba.fei.uim.oop.reversi;
-
-// public enum Direction {
-//     UP(0, -1),
-//     DOWN(0, 1),
-//     LEFT(-1, 0),
-//     RIGHT(1, 0),
-//     UPLEFT(-1, -1),
-//     UPRIGHT(1, -1),
-//     DOWNLEFT(-1, 1),
-//     DOWNRIGHT(1, 1);
-
-//     // private int x;
-//     // private int y;
-
-//     Direction(int x, int y) {
-//         // this.x = x;
-//         // this.y = y;
-//     }
-// }
-
 package sk.stuba.fei.uim.oop.reversi;
 
 public enum Direction {
